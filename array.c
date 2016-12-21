09c9c8b4f4836ab9548dcf8f951f955eca80dcd1
ZhuXiaoran07 <zhuxrsandra@163.com>
2016-12-21 16:32:16 -0600

    Update array.c

--- array.c
+++ array.c
@@ -1,9 +1,10 @@
 int n,i;
 print("The length of the array:");
-//n=read();
-n=SymInt;
-i=SymInt;
-while(i<n -1){
-	//a[i]=i;
+n=read();
+//n=#symInt;
+//i=#symInt;
+int a[n];
+while(i<n){
+	a[i]=i;
 	i=i+1;
 }
